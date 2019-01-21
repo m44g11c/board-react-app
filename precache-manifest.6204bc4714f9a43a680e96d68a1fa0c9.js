051bbfb9f5f912c3cbf947ca71b279a2db80e3e8
m44g11c <m44g11c@gmail.com>
2019-01-21 13:49:04 +0300

    Updates

---
+++ precache-manifest.6204bc4714f9a43a680e96d68a1fa0c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b8c9a9ce68568f4586a",
+    "url": "/board-react-app/static/js/runtime~main.7b8c9a9c.js"
+  },
+  {
+    "revision": "f09eedfe07e562f70c9b",
+    "url": "/board-react-app/static/js/main.f09eedfe.chunk.js"
+  },
+  {
+    "revision": "0609211363f485127e40",
+    "url": "/board-react-app/static/js/1.06092113.chunk.js"
+  },
+  {
+    "revision": "f09eedfe07e562f70c9b",
+    "url": "/board-react-app/static/css/main.f873ae12.chunk.css"
+  },
+  {
+    "revision": "0bb161dc2878bf74228fa955ddc1b97c",
+    "url": "/board-react-app/index.html"
+  }
+];
