e9a96d7f217104844d5db0cf4360e1f42ec82985
m44g11c <m44g11c@gmail.com>
2019-01-24 18:06:29 +0700

    Add info

---
+++ README.md
@@ -0,0 +1,12 @@
+# react bulletin board app
+
+* react version 16.7.0
+* html framework: bootstrap
+* backend: firebase
+
+# Screens:
+
+Main Page: ![Main Page](http://dl3.joxi.net/drive/2019/01/24/0013/3105/871457/57/c16faaee2b.png)
+Add advert Page: ![Add advert Page](http://dl3.joxi.net/drive/2019/01/24/0013/3105/871457/57/2c7fa9b8b4.png)
+Info & Action Page: ![Info & Action Page](http://dl4.joxi.net/drive/2019/01/24/0013/3105/871457/57/4215cd994b.png)
+Edit Advert: ![Edit Advert](http://dl3.joxi.net/drive/2019/01/24/0013/3105/871457/57/46c469a57b.png)
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 const config = {
-    apiKey: "AIzaSyD1NKCYuukpmgvKibt_67VES71lbIr3t3k",
-    authDomain: "firetest-9bb4f.firebaseapp.com",
-    databaseURL: "https://firetest-9bb4f.firebaseio.com",
-    projectId: "firetest-9bb4f",
-    storageBucket: "firetest-9bb4f.appspot.com",
-    messagingSenderId: "567060125936"
+    apiKey: "apiKey",
+    authDomain: "databaseURL",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId"
 };
 firebase.initializeApp(config);
 export default firebase; 
